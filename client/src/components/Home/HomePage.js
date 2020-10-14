3c5177625dee6cf8b53664a0896b9dad5528dd48
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-10-14 16:29:21 -0400

    created basic home page layout

---
+++ client/src/components/Home/HomePage.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+import Container from '@material-ui/core/Container'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Grid from '@material-ui/core/Grid'
+
+import GitHubIcon from '@material-ui/icons/GitHub';
+import YouTubeIcon from '@material-ui/icons/YouTube';
+
+export default class HomePage extends Component {
+    render() {
+        return (
+            <Container >
+                <Paper elevation={5} style={{ height: '100%', marginTop: '6rem', padding: '5rem' }}>
+                    <Typography variant='h1' align='center' style={{ paddingTop: '0.5em' }}>Mindful</Typography>
+                    <Typography variant='overline' display='block' align='center' style={{ marginTop: '1em', marginBottom: '5em' }}> Quote </Typography>
+
+                    <Grid container spacing={2}>
+                        <Grid item xs={1} sm={2} md={3} lg={4} xl={5}></Grid>
+                        <Grid item xs={5} sm={4} md={3} lg={2} xl={1} align='center'>
+                            <Button variant='outlined' size='large' >Sign In</Button>
+                        </Grid>
+                        <Grid item xs={5} sm={4} md={3} lg={2} xl={1} align='center' style={{ borderLeft: 'solid' }} >
+                            <Button variant='outlined' size='large' >Sign Up</Button>
+                        </Grid>
+                        <Grid item xs={1} sm={2} md={3} lg={4} xl={5}></Grid>
+                    </Grid>
+                </Paper>
+                <div style={{ marginTop: '1rem' }}>
+                    <GitHubIcon />
+                    <YouTubeIcon />
+                </div>
+            </Container>
+        )
+    }
+}
---
+++ client/src/containers/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import HomePage from '../components/Home/HomePage'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div >
+                <HomePage />
+            </div>
+        )
+    }
+}
